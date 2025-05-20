9b7bc104ce36d8967efae8d44b5ccb1b49d65791
MichaelSRD <87619724+MichaelSRD@users.noreply.github.com>
2025-05-20 12:43:16 -0500

    Update layout.tsx
    cambio de color de texto

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${dm_sans.className} antialiased bg-[#040711] ` }
+        className={`${dm_sans.className} antialiased bg-[#040711] text-white ` }
       >
         {children}
       </body>
